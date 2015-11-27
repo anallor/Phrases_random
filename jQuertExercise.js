9604a38ee8e5139cc487c0b0bc2a639946a36bc0
Ana Llorca <ana.llorca.a@gmail.com>
2015-11-27 09:53:01 +0100

    phrases_random

---
+++ jQuertExercise.js
@@ -0,0 +1,4 @@
+ var phrases = ['Frankly my dear, I do not give a damn', 'May the Force be with you.', 
+    'E.T. Phone home.', 'Well, nobody is perfect.'];
+    var show = phrases[Math.floor(Math.random() * phrases.length)];
+    document.write(show)
